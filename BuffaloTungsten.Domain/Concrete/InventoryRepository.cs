fe30110746aa93f0f180ad54f1c85d5ecd7dc2ae
elemental21 <chris@cablesimple.com>
2015-07-28 00:01:23 -0400

    v.4 * Added upload action to accept Excel file for inventory.  It doesn't do anything yet however, just opens and reads the file.

---
+++ BuffaloTungsten.Domain/Concrete/InventoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using BuffaloTungsten.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuffaloTungsten.Domain.Concrete
+{
+    class InventoryRepository : IInventoryRepository
+    {
+        public IEnumerable<Entities.Inventory> GetInventoryByLot(string lotNumber, string lotType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BuffaloTungsten/Controllers/InventoryController.cs
@@ -0,0 +1,42 @@
+﻿using Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuffaloTungsten.Controllers
+{
+    public class InventoryController : Controller
+    {
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Upload(HttpPostedFileBase file)
+        {
+            //FileStream stream = System.IO.File.Open(@"c:\W_LotInv.xlsx", FileMode.Open, FileAccess.Read);
+            IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(file.InputStream);
+            string data;
+            while (excelReader.Read())
+            {
+                data = excelReader.GetString(0);
+            }
+
+            //6. Free resources (IExcelDataReader is IDisposable)
+            excelReader.Close();
+
+            return View();
+        }
+
+    }
+}
